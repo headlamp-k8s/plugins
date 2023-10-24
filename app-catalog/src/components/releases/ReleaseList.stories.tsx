5b7ad00b2b7f76032524f51b53950b17c9071a11
René Dudfield <renedudfield@microsoft.com>
2023-10-24 11:04:46 +0200

    app-catalog: Add ReleaseList.stories
    Signed-off-by: René Dudfield <renedudfield@microsoft.com>

---
+++ app-catalog/src/components/releases/ReleaseList.stories.tsx
@@ -0,0 +1,69 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { Meta, Story } from '@storybook/react/types-6-0';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import ReleaseList from './List';
+
+export default {
+  title: 'components/release/List',
+  component: ReleaseList,
+} as Meta;
+
+const mockReleases = [
+  {
+    name: 'example-release-1',
+    namespace: 'default',
+    chart: {
+      metadata: {
+        name: 'example-chart-1',
+        appVersion: '1.0.0',
+        icon: 'https://via.placeholder.com/50',
+      },
+    },
+    version: '1.0',
+    info: {
+      status: 'deployed',
+      last_deployed: '2023-10-23T12:00:00Z',
+    },
+  },
+  {
+    name: 'example-release-2',
+    namespace: 'default',
+    chart: {
+      metadata: {
+        name: 'example-chart-2',
+        appVersion: '2.0.0',
+        icon: 'https://via.placeholder.com/50',
+      },
+    },
+    version: '2.0',
+    info: {
+      status: 'error',
+      last_deployed: '2023-10-24T12:00:00Z',
+    },
+  },
+];
+
+const initialState = {
+  config: {
+    settings: {
+      tableRowsPerPageOptions: [15, 25, 50],
+    },
+  },
+};
+
+const mockStore = configureStore({
+  reducer: (state = initialState) => state,
+});
+
+const Template: Story = args => (
+  <Provider store={mockStore}>
+    <BrowserRouter>
+      <ReleaseList {...args} fetchReleases={() => Promise.resolve({ releases: mockReleases })} />
+    </BrowserRouter>
+  </Provider>
+);
+
+export const Default = Template.bind({});
+Default.args = {};
