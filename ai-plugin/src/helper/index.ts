ed5dd9e6e1a446d0293956fdafa88c06262e66ab
Joaquim Rocha <joaquim.rocha@microsoft.com>
2024-03-26 17:11:19 +0000

    Remove ai-plugin from main
    This was committed by mistake. The ai-plugin will be available but it
    is still not in a state where we want to merge it.
    
    Signed-off-by: Joaquim Rocha <joaquim.rocha@microsoft.com>

--- ai-plugin/src/helper/index.ts
+++
@@ -1,5 +0,0 @@
-export const formatString = function (a: string, ...args: any) {
-  return a.replace(/{(\d+)}/g, function (match, number) {
-    return typeof args[number] != 'undefined' ? args[number] : match;
-  });
-};
