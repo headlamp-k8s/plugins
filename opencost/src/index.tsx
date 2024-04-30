02f2a0706db8ca234d87e042f5a516b026420554
yolossn <sannagaraj@microsoft.com>
2024-04-30 12:31:39 +0530

    plugins: Add opencost plugin
    this patch adds opencost plugin
    
    Signed-off-by: yolossn <sannagaraj@microsoft.com>

---
+++ opencost/src/index.tsx
@@ -0,0 +1,147 @@
+import './loader.css';
+import { Icon } from '@iconify/react';
+import {
+  registerDetailsViewHeaderAction,
+  registerDetailsViewSectionsProcessor,
+  registerPluginSettings,
+  registerResourceTableColumnsProcessor,
+} from '@kinvolk/headlamp-plugin/lib';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import { useEffect, useState } from 'react';
+import { useBetween } from 'use-between';
+import { OpencostDetailSection } from './detail';
+import { fetchOpencostData } from './request';
+import { Settings } from './settings';
+import { getConfigStore, getDisplayCurrency, getDisplayTimespan, getServiceDetails } from './utils';
+
+function IconAction() {
+  return (
+    <Tooltip title={'Cost'}>
+      <IconButton href="#cost" aria-label={'Cost'} aria-haspopup="true">
+        <Icon icon="mdi:cash-register" />
+      </IconButton>
+    </Tooltip>
+  );
+}
+
+registerDetailsViewHeaderAction(IconAction);
+
+registerPluginSettings('opencost', Settings, true);
+
+registerDetailsViewSectionsProcessor(function addSubheaderSection(resource, sections) {
+  if (
+    resource?.kind === 'Pod' ||
+    resource?.kind === 'Node' ||
+    resource?.kind === 'Namespace' ||
+    resource?.kind === 'Service' ||
+    resource?.kind === 'Deployment'
+  ) {
+    sections.push({
+      title: 'Opencost',
+      section: <OpencostDetailSection resource={resource} type={resource?.kind.toLowerCase()} />,
+    });
+  }
+
+  return sections;
+});
+
+const configStore = getConfigStore();
+
+let config = configStore.get();
+
+if (config?.isEnabledInListView === undefined) {
+  config = { ...config, isEnabledInListView: true };
+}
+
+if (config?.isEnabledInListView) {
+  const acceptedList = [
+    'headlamp-pods',
+    'headlamp-nodes',
+    'headlamp-namespaces',
+    'headlamp-services',
+    'headlamp-deployments',
+  ];
+
+  function resourceTypeFromId(id: string) {
+    switch (id) {
+      case 'headlamp-pods':
+        return 'pod';
+      case 'headlamp-nodes':
+        return 'node';
+      case 'headlamp-namespaces':
+        return 'namespace';
+      case 'headlamp-services':
+        return 'service';
+      case 'headlamp-deployments':
+        return 'deployment';
+      default:
+        return '';
+    }
+  }
+
+  const useOpenCostData = () => {
+    const [data, setData] = useState(null);
+    const [isLoading, setIsLoading] = useState<boolean | null>(null);
+    const [resourceType, setResourceType] = useState('');
+
+    useEffect(() => {}, [resourceType]);
+    useEffect(() => {}, [isLoading]);
+
+    useEffect(() => {
+      if (resourceType === '') {
+        return;
+      }
+      setIsLoading(true);
+      const fetchData = async () => {
+        const [namespace, serviceName] = await getServiceDetails();
+        const result = await fetchOpencostData(
+          namespace,
+          serviceName,
+          getDisplayTimespan(),
+          resourceType,
+          true
+        );
+        setData(result);
+        setIsLoading(false);
+      };
+
+      fetchData();
+    }, [resourceType]);
+
+    return { data, isLoading, setResourceType };
+  };
+
+  const useSharedData = () => useBetween(useOpenCostData);
+
+  function OpenCostData(props: { resource; resourceType }) {
+    const { resource, resourceType } = props;
+    const { data, isLoading, setResourceType } = useSharedData();
+
+    if (isLoading === null) {
+      setResourceType(resourceType);
+    } else if (isLoading) {
+      return <>{`...`}</>;
+    } else {
+      const totalCost = data?.data[0]?.[resource.jsonData?.metadata?.name]?.totalCost ?? 0;
+      return <>{totalCost ? `${getDisplayCurrency()} ${totalCost}` : 'N/A'}</>;
+    }
+  }
+
+  registerResourceTableColumnsProcessor(function processor({ id, columns }) {
+    if (acceptedList.includes(id)) {
+      const filteredColumns = columns.filter(col => col.id !== 'opencost');
+      const column = {
+        id: 'opencost',
+        label: `Cost (${getDisplayTimespan()})`,
+        getter: resource => {
+          return <OpenCostData resource={resource} resourceType={resourceTypeFromId(id)} />;
+        },
+        show: true,
+      };
+      filteredColumns.splice(filteredColumns.length - 1, 0, column);
+      return filteredColumns;
+    }
+    return columns;
+  });
+}
