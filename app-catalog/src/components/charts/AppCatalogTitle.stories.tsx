95b2746c0dac10df45425e675189f47700c090cc
Vincent T <vtaylor@microsoft.com>
2024-08-28 15:32:58 -0400

    app-catalog: Fix stories for AppCatalogTitle and SettingsLink
    Signed-off-by: Vincent T <vtaylor@microsoft.com>

--- app-catalog/src/components/charts/AppCatalogTitle.stories.tsx
+++ app-catalog/src/components/charts/AppCatalogTitle.stories.tsx
@@ -1,10 +1,12 @@
 import { Meta, Story } from '@storybook/react';
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { AppCatalogTitle } from './AppCatalogTitle';
 
 export default {
     title: 'Components/AppCatalogTitle',
     component: AppCatalogTitle,
+    decorators: [(Story) => <Router><Story /></Router>],
 } as Meta;
 
 const Template: Story = (args) => <AppCatalogTitle {...args} />;
--- app-catalog/src/components/charts/SettingsLink.stories.tsx
+++ app-catalog/src/components/charts/SettingsLink.stories.tsx
@@ -1,10 +1,13 @@
 import { Meta, Story } from '@storybook/react';
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { SettingsLink } from './SettingsLink';
+
 
 export default {
     title: 'Components/SettingsLink',
     component: SettingsLink,
+    decorators: [(Story) => <Router><Story /></Router>],
 } as Meta;
 
 const Template: Story = (args) => <SettingsLink {...args} />;
