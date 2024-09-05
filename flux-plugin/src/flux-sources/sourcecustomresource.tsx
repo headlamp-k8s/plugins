84faf800b2c8baefdd4af1eaa6ca0576a6632834
ashu8912 <aghildiyal@microsoft.com>
2024-09-05 13:12:56 +0530

    Make source follow what flux cli shows
    Signed-off-by: ashu8912 <aghildiyal@microsoft.com>

--- flux-plugin/src/flux-sources/sourcecustomresource.tsx
+++ flux-plugin/src/flux-sources/sourcecustomresource.tsx
@@ -10,96 +10,125 @@ export default function FluxSourceCustomResource(props: {
 }) {
   const { resourceClass, title, type } = props;
   const [resource] = resourceClass.useList();
-  console.log(resource);
+
+  function prepareColumns() {
+    const columns = [
+      {
+        header: 'Name',
+        accessorFn: item => {
+          return (
+            <Link
+              routeName={`/flux/${type}/:namespace/:type/:name`}
+              params={{
+                name: item.metadata.name,
+                namespace: item.metadata.namespace,
+                type: title.split(' ').join('').toLowerCase(),
+              }}
+            >
+              {' '}
+              {item.metadata.name}{' '}
+            </Link>
+          );
+        },
+      },
+      {
+        header: 'Namespace',
+        accessorFn: item => (
+          <Link
+            routeName="namespace"
+            params={{
+              name: item.metadata.namespace,
+            }}
+          >
+            {item.metadata.namespace}
+          </Link>
+        ),
+      },
+
+      {
+        header: 'Age',
+        accessorFn: item => <DateLabel date={item.metadata.creationTimestamp} />,
+      },
+      {
+        header: 'Ready',
+        accessorFn: item => {
+          const ready = item.jsonData.status?.conditions?.find(c => c.type === 'Ready')?.status;
+          return ready || '-';
+        },
+      },
+      {
+        header: 'Status',
+        accessorFn: item => {
+          const message = item.jsonData.status?.conditions?.find(
+            c => c.type === 'Ready'
+          )?.message;
+          return message || '-';
+        },
+      },
+    ];
+
+    return columns;
+  }
+  console.log("resource is ",resource);
+  const isHelmChart = resource?.[0]?.jsonData?.kind === 'HelmChart';
+  const columns = prepareColumns();
+  if(isHelmChart) {
+    console.log("resource is HelmChart");
+    // add chart column to second index
+    columns.splice(2, 0, {
+      header: 'Chart',
+      accessorFn: item => {
+        const chart = item?.jsonData?.spec?.chart;
+        return chart || '-';
+      },
+    });
+
+    // add Version  column to third index
+    columns.splice(3, 0, {
+      header: 'Version',
+      accessorFn: item => {
+        const version = item?.jsonData?.spec?.version;
+        return version || '-';
+      },
+    });
+
+    // add source kind column to fourth index
+
+    columns.splice(4, 0, {
+      header: 'Source Kind',
+      accessorFn: item => {
+        const sourceKind = item?.jsonData?.spec?.sourceRef.kind;
+        return sourceKind || '-';
+      },
+    });
+
+    // add source name column to fifth index
+
+    columns.splice(5, 0, {
+      header: 'Source Name',
+      accessorFn: item => {
+        const sourceName = item?.jsonData?.spec?.sourceRef.name;
+        if(sourceName) {
+          return <Link routeName={`/flux/sources/:namespace/:type/:name`} params={{ namespace: item.jsonData.metadata.namespace, type: item.jsonData.spec.sourceRef.kind, name: sourceName }}>{sourceName}</Link>;
+        }
+        return '-';
+      },
+    });
+  } else {
+    columns.splice(2, 0, {
+      header: 'URL',
+      accessorFn: item => {
+        const url = item?.jsonData?.spec?.url;
+        return url || '-';
+      },
+    })
+  }
+  console.log("columns is ",columns);
   return (
     <SectionBox title={title}>
       <Table
         data={resource}
-        columns={[
-          {
-            header: 'Name',
-            accessorFn: item => {
-              console.log(item);
-              return (
-                <Link
-                  routeName={`/flux/${type}/:namespace/:type/:name`}
-                  params={{
-                    name: item.metadata.name,
-                    namespace: item.metadata.namespace,
-                    type: title.split(' ').join('').toLowerCase(),
-                  }}
-                >
-                  {' '}
-                  {item.metadata.name}{' '}
-                </Link>
-              );
-            },
-          },
-          {
-            header: 'Namespace',
-            accessorFn: item => (
-              <Link
-                routeName="namespace"
-                params={{
-                  name: item.metadata.namespace,
-                }}
-              >
-                {item.metadata.namespace}
-              </Link>
-            ),
-          },
-          {
-            header: 'Ready',
-            accessorFn: item => {
-              const ready =
-                item.jsonData.status?.conditions?.findIndex(c => c.type === 'Ready') !== -1
-                  ? 'True'
-                  : 'False';
-              return ready;
-            },
-          },
-          {
-            header: 'Message',
-            accessorFn: item => {
-              const message = item.jsonData.status?.conditions?.find(
-                c => c.type === 'Ready'
-              )?.message;
-              return message || '-';
-            },
-          },
-          {
-            header: 'URL',
-            accessorFn: item => {
-              const url = item.jsonData.spec?.url;
-              return <MuiLink href={url}> {url} </MuiLink> || '-';
-            },
-          },
-          {
-            header: 'Chart',
-            accessorFn: item => {
-              const chart = item.jsonData.spec?.chart;
-              return chart || '-';
-            },
-          },
-          {
-            header: 'Reference',
-            accessorFn: item => {
-              const reference = item.jsonData.spec?.ref?.branch;
-              return reference || '-';
-            },
-          },
-          {
-            header: 'Interval',
-            accessorFn: item => {
-              const interval = item.jsonData.spec?.interval;
-              return interval || '-';
-            },
-          },
-          {
-            header: 'Last Updated',
-            accessorFn: item => <DateLabel date={item.metadata.creationTimestamp} />,
-          },
-        ]}
+        columns={columns}
       />
     </SectionBox>
   );
