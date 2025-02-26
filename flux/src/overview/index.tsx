acb72287c5f9fd67b98d9c7546f104c602fb4988
ashu8912 <aghildiyal@microsoft.com>
2025-02-26 13:46:49 +0530

    flux overview page
    Signed-off-by: ashu8912 <aghildiyal@microsoft.com>

---
+++ flux/src/overview/index.tsx
@@ -0,0 +1,8 @@
+export function FluxOverview() {
+    return (
+        <div className="overview">
+        <h1>Flux Overview</h1>
+        <p>Flux is a powerful tool for managing and analyzing data in your applications.</p>
+        </div>
+    );
+}
