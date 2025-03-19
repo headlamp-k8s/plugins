52f6497ee3b179e6412a866af5a62c25581d5cdd
René Dudfield <renedudfield@microsoft.com>
2025-03-19 13:53:18 +0100

    app-catalog: Upgrade to headlamp-plugin 0.11.0-alpha.3
    Signed-off-by: René Dudfield <renedudfield@microsoft.com>

--- app-catalog/src/headlamp-plugin.d.ts
+++ app-catalog/src/headlamp-plugin.d.ts
@@ -1,6 +1,1 @@
 /// <reference types="@kinvolk/headlamp-plugin" />
-
-declare module '*.svg' {
-  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
-  export default content;
-}
