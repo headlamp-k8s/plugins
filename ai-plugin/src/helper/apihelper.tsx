77669d0520171738c15222d5140a3e67cda04211
ashu8912 <aghildiyal@microsoft.com>
2025-07-23 07:13:44 +0530

    ai-plugin: Fix yaml editor mounting on scroll
    Signed-off-by: ashu8912 <aghildiyal@microsoft.com>

--- ai-plugin/src/helper/apihelper.tsx
+++ ai-plugin/src/helper/apihelper.tsx
@@ -1,5 +1,5 @@
 import { clusterAction } from '@kinvolk/headlamp-plugin/lib';
-import { apply, clusterRequest, request } from '@kinvolk/headlamp-plugin/lib/ApiProxy';
+import { apply, clusterRequest } from '@kinvolk/headlamp-plugin/lib/ApiProxy';
 import { getCluster } from '@kinvolk/headlamp-plugin/lib/Utils';
 import YAML from 'yaml';
 import { getAppUrl, isLogRequest, isSpecificResourceRequestHelper } from '.';
