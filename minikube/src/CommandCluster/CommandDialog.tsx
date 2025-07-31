38cacb1b85abea374574730b6fb5dbc722776fcd
René Dudfield <renedudfield@microsoft.com>
2025-07-31 23:27:33 +0200

    minikube: CommandDialog: Fix cluster name input so can type again
    There was a bug where the initial name is set, but you can't
    change it.
    
    Signed-off-by: René Dudfield <renedudfield@microsoft.com>

--- minikube/src/CommandCluster/CommandDialog.tsx
+++ minikube/src/CommandCluster/CommandDialog.tsx
@@ -68,10 +68,12 @@ export default function CommandDialog({
   const clusterNames = Object.keys(clusters);
 
   React.useEffect(() => {
-    if (!initialClusterName) {
+    if (open && !initialClusterName && askClusterName) {
       setClusterName(generateClusterName(clusterNames));
     }
-  }, [initialClusterName, clusterNames]);
+    // Only generate a new name when dialog is opened, not on every clusterNames change
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [open, initialClusterName, askClusterName]);
 
   function generateClusterName(existingNames: string[]): string {
     const baseName = 'minikube';
