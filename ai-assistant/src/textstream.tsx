aab0b6d08436282c67939d269f2787e69c21ac25
René Dudfield <renedudfield@microsoft.com>
2025-11-21 10:34:49 -0300

    ai-assistant: Upgrade headlamp-plugin 0.13.0-alpha.14
    Also fix a ts issue.
    
    Signed-off-by: René Dudfield <renedudfield@microsoft.com>

--- ai-assistant/src/textstream.tsx
+++ ai-assistant/src/textstream.tsx
@@ -7,18 +7,18 @@ import { Prompt } from './ai/manager';
 import ContentRenderer from './ContentRenderer';
 import EditorDialog from './editordialog';
 
-// declare module '@mui/material/styles' {
-//   interface Palette {
-//     sidebar: {
-//       selectedBackground: string;
-//     };
-//   }
-//   interface PaletteOptions {
-//     sidebar: {
-//       selectedBackground: string;
-//     };
-//   }
-// }
+declare module '@mui/material/styles' {
+  interface Palette {
+    sidebar: {
+      selectedBackground: string;
+    };
+  }
+  interface PaletteOptions {
+    sidebar: {
+      selectedBackground: string;
+    };
+  }
+}
 
 const TextStreamContainer = React.memo(function TextStreamContainer({
   history,
