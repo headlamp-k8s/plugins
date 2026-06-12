04320abb05631741aeb3623177b6f2f03879f38d
skools-here <sajalkulshreshtha9@gmail.com>
2026-06-12 23:09:51 +0530

    releaser: Configure Vitest test infrastructure
    Signed-off-by: skools-here <sajalkulshreshtha9@gmail.com>

--- tools/releaser/vitest.config.ts
+++ tools/releaser/vitest.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'node',
+    restoreMocks: true,
     include: ['src/**/*.test.ts'],
     coverage: {
       provider: 'v8',
