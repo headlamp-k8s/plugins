603a910eff963f8a4b4afcec164e00cf86acc06f
Evangelos Skopelitis <eskopelitis@microsoft.com>
2026-06-30 17:55:31 -0400

    plugin-catalog: Fix plugin card heading order
    Signed-off-by: Evangelos Skopelitis <eskopelitis@microsoft.com>

--- plugin-catalog/src/components/plugins/PluginCard.tsx
+++ plugin-catalog/src/components/plugins/PluginCard.tsx
@@ -97,7 +97,7 @@ export function PluginCard(props: PluginCardProps) {
             }}
           >
             <Typography
-              component="h5"
+              component="div"
               variant="h5"
               sx={{
                 overflow: 'hidden',
