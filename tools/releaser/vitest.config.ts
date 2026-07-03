ee69152eaf5f89d5099d2fd0ea785b1af46b2d7c
skools-here <sajalkulshreshtha9@gmail.com>
2026-07-03 18:06:15 +0530

    releaser: Address inquirer types and test coverage
    Signed-off-by: skools-here <sajalkulshreshtha9@gmail.com>

--- tools/releaser/vitest.config.ts
+++ tools/releaser/vitest.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
-      include: ['src/**/*.ts'],
+      include: ['src/utils/**/*.ts'],
       exclude: ['src/**/*.test.ts'],
     },
   },
