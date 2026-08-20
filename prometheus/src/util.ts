1ae4de361746e720a52d768f77dda61f07f99ff5
Joshna907 <joshnawaikar@gmail.com>
2026-08-20 15:34:59 +0530

    prometheus: util: Support Argo CD Application metrics
    Signed-off-by: Joshna907 <joshnawaikar@gmail.com>

--- prometheus/src/util.ts
+++ prometheus/src/util.ts
@@ -164,6 +164,7 @@ function getResourceApiVersion(resource?: ResourceIdentity): string | undefined 
 const resourceApiVersionRules: Record<string, RegExp> = {
   Job: /^batch\/v1$/,
   Queue: /^scheduling\.volcano\.sh\/v1beta1$/,
+  Application: /^argoproj\.io\/v1alpha1$/,
   Cluster: /^cluster\.x-k8s\.io\//,
   Machine: /^cluster\.x-k8s\.io\//,
   MachineDeployment: /^cluster\.x-k8s\.io\//,
